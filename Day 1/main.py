21fd34dc06859fe19f65156e1bfb41c5f91f541f
krn <contadorkrn@gmail.com>
2023-04-12 14:31:33 -0300

    Branch published

---
+++ Day 1/main.py
@@ -0,0 +1,15 @@
+# 6. Printing to the Console in Python
+# print("Hello world!")
+
+
+# 8. [Interactive Coding Exercise] Printing
+# CodingRoom
+
+# #Write your code below this line 👇
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+# print("Hello world!\nHello world!\nHello world!")
+
+print("Hello" + " Angela")
