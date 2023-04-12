87adfff681b23463dd66d0e3f181c33bcc25dfbf
krn <contadorkrn@gmail.com>
2023-04-12 14:58:13 -0300

    Lesson 11

--- Day 1/main.py
+++ Day 1/main.py
@@ -10,6 +10,21 @@
 # print("The function is declared like this:")
 # print("print('what to print')")
 
+
+# 9. String Manipulation and Code Intelligence
 # print("Hello world!\nHello world!\nHello world!")
 
-print("Hello" + " Angela")
+# print("Hello" + " Angela")
+
+
+# 10. [Interactive Coding Exercise] Debugging Practice
+# CodingRoom
+
+# Fix the code below 👇
+
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+# 11. The Python Input Function
