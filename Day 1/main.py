994cf346662d246bce8409bce23f1a012469ef9d
krn <contadorkrn@gmail.com>
2023-04-12 23:31:55 -0300

    Lesson 14

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,5 +1,6 @@
 # 6. Printing to the Console in Python
 # print("Hello world!")
+# -------------------------------------
 
 
 # 8. [Interactive Coding Exercise] Printing
@@ -9,12 +10,14 @@
 # print("Day 1 - Python Print Function")
 # print("The function is declared like this:")
 # print("print('what to print')")
+# -------------------------------------
 
 
 # 9. String Manipulation and Code Intelligence
 # print("Hello world!\nHello world!\nHello world!")
 
 # print("Hello" + " Angela")
+# -------------------------------------
 
 
 # 10. [Interactive Coding Exercise] Debugging Practice
@@ -26,5 +29,60 @@
 # print('String Concatenation is done with the "+" sign.')
 # print('e.g. print("Hello " + "world")')
 # print("New lines can be created with a backslash and n.")
+# -------------------------------------
+
 
 # 11. The Python Input Function
+
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+# print("Hello " + input("What is your name? ") + "!")
+# -------------------------------------
+
+
+# 12. [Interactive Coding Exercise] Input Function
+
+#Write your code below this line 👇
+# print(len(input("What is your name? ")))
+# -------------------------------------
+
+
+# 13. Python Variables
+
+# name = input("What is your name? ")
+# print(name)
+
+# name = 'Jack'
+# print(name)
+
+# name = 'Angela'
+# print(name)
+
+# name = input('What is your name? ')
+# lenght = len(name)
+# print(lenght)
+# -------------------------------------
+
+
+# 14. [Interactive Coding Exercise] Variables
+# 🚨 Don't change the code below 👇
+# a = input("a: ")
+# b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+# a2 = b
+# b2 = a
+# a = a2
+# b = b2
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
+# -------------------------------------
+
+
