597cfa65f443e38d90b8c6b5207fdecefd15c54c
krn <contadorkrn@gmail.com>
2023-04-13 14:03:04 -0300

    Day 2 - Lesson 22

---
+++ Day 2/main.py
@@ -0,0 +1,53 @@
+# 19. Python Primitive Data Types
+
+# Data Types
+
+# String
+# print("Hello"[0]) -> Subscripting
+# print("Hello"[4])
+# print("123" + "345")
+
+# Integer
+# print(123 + 345)
+# 123_456_789
+
+# Float
+# 3.14159
+
+# Boolean
+# True
+# False
+# -------------------------------------
+
+
+# 20. Type Error, Type Checking and Type Conversion
+
+# num_char = len(input("What is your name? "))
+# print("Your name has " + num_char + " characters.")
+# print(type(num_char))
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters.")
+
+# a = float(123)
+# print(type(a))
+# print(70 + float("100.5"))
+# print(str(70) + str(100))
+# -------------------------------------
+
+
+# 21. [Interactive Coding Exercise] Data Types
+
+# 🚨 Don't change the code below 👇
+# two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+# digit_1 = int(two_digit_number[0])
+# digit_2 = int(two_digit_number[1])
+# sum_digits = digit_1 + digit_2
+# print(sum_digits)
+# -------------------------------------
+
+
+# 22. Mathematical Operations in Python
