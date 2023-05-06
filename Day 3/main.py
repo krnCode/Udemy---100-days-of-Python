eb63141df7a6939a240be877c75f986594594de4
krn <contadorkrn@gmail.com>
2023-05-06 11:41:49 -0300

    Day 3 Lesson 37

--- Day 3/main.py
+++ Day 3/main.py
@@ -117,3 +117,118 @@
 # else:
 #     print("Sorry, you have to grow taller before you can ride.")
 # -------------------------------------
+
+
+# 35. [Interactive Coding Exercise] Pizza Order Practice
+
+# # 🚨 Don't change the code below 👇
+# print("Welcome to Python Pizza Deliveries!")
+# size = input("What size pizza do you want? S, M, or L ")
+# add_pepperoni = input("Do you want pepperoni? Y or N ")
+# extra_cheese = input("Do you want extra cheese? Y or N ")
+# # 🚨 Don't change the code above 👆
+
+# # Write your code below this line 👇
+
+# # Total bill
+# pizzaTotal = 0
+
+# # Pizza prices
+# pizzaSmallPrice = 15
+# pizzaMediumPrice = 20
+# pizzaLargePrice = 25
+
+
+# # Toppings prices
+# pepperoniSmall = 2
+# pepperoniMedium = 3
+# extraCheesePrice = 1
+
+
+# # Pizza Price Calculation Logic
+# if size == "S":
+#     pizzaTotal += pizzaSmallPrice
+# elif size == "M":
+#     pizzaTotal += pizzaMediumPrice
+# else:
+#     pizzaTotal += pizzaLargePrice
+
+
+# # Toppings Calculation Logic
+# if add_pepperoni == "Y" and size == "S":
+#     pizzaTotal += pepperoniSmall
+# elif add_pepperoni == "Y" and size == "M":
+#     pizzaTotal += pepperoniMedium
+# elif add_pepperoni == "Y" and size == "L":
+#     pizzaTotal += pepperoniMedium
+# else:
+#     pizzaTotal
+
+# if extra_cheese == "Y":
+#     pizzaTotal += extraCheesePrice
+
+
+# print(f'Your final bill is: ${pizzaTotal}.')
+# -------------------------------------
+
+
+#  36. Logical Operators
+
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your hehight in cm? "))
+
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+
+#     age = int(input("What is your age? "))
+#     if age < 12:
+#         bill = 5
+#         print("Child tickets are $5.")
+#     elif age <= 18:
+#         bill = 7
+#         print("Youth tickets are $7.")
+#     elif age >= 45 and age <= 55:
+#         bill = 0
+#         print("Youth tickets are $0.")
+#     else:
+#         bill = 12
+#         print("Adult tickets are pay $12.")
+    
+#     wants_photo = input("Do you want a photo taken? Y or N. ")
+
+#     if wants_photo == "Y":
+#         # Add $3 to their bill 
+#         bill += 3
+
+#         print(f"Your final bill is ${bill}")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+# -------------------------------------
+
+
+# 37. [Interactive Coding Exercise] Love Calculator
+
+# # 🚨 Don't change the code below 👇
+# print("Welcome to the Love Calculator!")
+# name1 = input("What is your name? \n")
+# name2 = input("What is their name? \n")
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+# name1Lower = name1.lower()
+# name2Lower = name2.lower()
+
+# counter1 = name1Lower.count("t") + name1Lower.count("r") + name1Lower.count("u") + name1Lower.count("e") + name2Lower.count("t") + name2Lower.count("r") + name2Lower.count("u") + name2Lower.count("e")
+# counter2 = name1Lower.count("l") + name1Lower.count("o") + name1Lower.count("v") + name1Lower.count("e") + name2Lower.count("l") + name2Lower.count("o") + name2Lower.count("v") + name2Lower.count("e")
+
+# counterTotal = int(str(counter1) + str(counter2))
+
+
+# if counterTotal < 10 or counterTotal > 90:
+#     print(f"Your score is {counterTotal}, you go together like coke and mentos.")
+
+# elif counterTotal >= 40 and counterTotal <= 50:
+#     print(f"Your score is {counterTotal}, you are alright together.")
+
+# else:
+#     print(f"Your score is {counterTotal}.")
