263d707b9abf96c5449be143c1fd1306be8454cf
krn <contadorkrn@gmail.com>
2023-06-01 15:36:56 -0300

    Day 4 Lesson 43

---
+++ Day 004/main.py
@@ -0,0 +1,56 @@
+# 41. Random Module
+
+# import random
+# import my_module
+
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+
+# # print(my_module.pi)
+
+# # random_float = random.random()
+# # print(random_float)
+
+# random_float2 = random.random() * 5
+# print(random_float2)
+
+# love_score = random.randint(1, 100)
+# print(f"Your love score is {love_score}")
+# -------------------------------------
+
+
+# 42. [Interactive Coding Exercise] Heads or Tails
+
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+	 
+#Write the rest of your code below this line 👇
+# import random
+
+# toss = random.randint(0, 1)
+
+# if toss == 1:
+#     print("Heads")
+# else:
+#     print("Tails")
+# -------------------------------------
+
+
+# 43. Understanding the Offset and Appending Items to Lists
+
+# states_of_america = ["Delaware","Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", 
+# "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine",
+# "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska",
+# "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+# print(states_of_america)
+
+# states_of_america[1] = "Pencilvania"
+# print(states_of_america)
+
+# states_of_america.append("Angelaland")
+# print(states_of_america)
+
+# states_of_america.extend(["Angelaland", "Jack Bauer Land"])
+# print(states_of_america)
+# -------------------------------------
