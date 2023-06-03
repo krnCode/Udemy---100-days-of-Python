861281656e7ff2586d0c0df934036c54c8b30554
krn <contadorkrn@gmail.com>
2023-06-03 17:21:10 -0300

    Day 4 Lesson 46

--- Day 004/main.py
+++ Day 004/main.py
@@ -54,3 +54,65 @@
 # states_of_america.extend(["Angelaland", "Jack Bauer Land"])
 # print(states_of_america)
 # -------------------------------------
+
+
+# 44. [Interactive Coding Exercise] Banker Roulette - Who will pay the bill?
+
+# # Import the random module here
+# import random
+# # Split string method
+# names_string = input("Give me everybody's names, separated by a comma. ")
+# names = names_string.split(", ")
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+# random_pick = random.randint(0, len(names) - 1)
+# print(f"{names[random_pick]} is going to buy meal today!")
+# -------------------------------------
+
+
+# 45. IndexErrors and Working with Nested Lists
+
+# states_of_america = ["Delaware","Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", 
+# "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine",
+# "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska",
+# "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+# num_of_states = len(states_of_america) - 1
+
+# print(states_of_america[num_of_states])
+
+# dirty_dozen = ["Strawberry", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+# fruits = ["Strawberry", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+# dirty_dozen = [fruits, vegetables]
+# print(dirty_dozen)
+# -------------------------------------
+
+
+# 46. [Interactive Coding Exercise] Treasure Map
+
+# # 🚨 Don't change the code below 👇
+# row1 = ["⬜️","️⬜️","️⬜️"]
+# row2 = ["⬜️","⬜️","️⬜️"]
+# row3 = ["⬜️️","⬜️️","⬜️️"]
+# map = [row1, row2, row3]
+# print(f"{row1}\n{row2}\n{row3}")
+# position = input("Where do you want to put the treasure? ")
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this row 👇
+# pos_list = int(position[1]) - 1
+# pos_item = int(position[0]) - 1
+
+# map[pos_list][pos_item] = " X" 
+
+# #Write your code above this row 👆
+
+# # 🚨 Don't change the code below 👇
+# print(f"{row1}\n{row2}\n{row3}")
+# -------------------------------------
+
+
