31192017d1c23853202cf5dea75bcf6fb26f4165
krn <contadorkrn@gmail.com>
2023-06-16 19:44:46 -0300

    Day 007 - HangmanGame project

--- Day 007/main.py
+++ Day 007/main.py
@@ -228,23 +228,28 @@
 
 #Step 5
 import random
+import hangman_words
+import hangman_art
 
 #TODO-1: - Update the word list to use the 'word_list' from
 # hangman_words.py
 #Delete this line: word_list = ["ardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
+chosen_word = random.choice(hangman_words.word_list)
 word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 
 #TODO-3: - Import the logo from hangman_art.py and print it
 # at the start of the game.
+print(hangman_art.logo)
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
 #Create blanks
 display = []
+wrong_letters = []
+
 for _ in range(word_length):
     display += "_"
 
@@ -257,7 +262,7 @@ while not end_of_game:
     #Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
-        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
 
@@ -265,6 +270,9 @@ while not end_of_game:
     if guess not in chosen_word:
         #TODO-5: - If the letter is not in the chosen_word, 
         # print out the letter and let them know it's not in the word.
+        print(f"The letter {guess} is not in the word.")
+        wrong_letters.append(guess)
+        print(f"Wrong letters: {', '.join(wrong_letters)}")
         lives -= 1
         if lives == 0:
             end_of_game = True
@@ -280,5 +288,5 @@ while not end_of_game:
 
     #TODO-2: - Import the stages from hangman_art.py and make
     # this error go away.
-    print(stages[lives])
+    print(hangman_art.stages[lives])
 # -------------------------------------    
