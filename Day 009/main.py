0c96d5a877b544cc396cc1fe4eefadf9bb90d54c
krn <contadorkrn@gmail.com>
2023-06-20 14:07:22 -0300

    Commented out some code on Lesson 92

--- Day 009/main.py
+++ Day 009/main.py
@@ -38,18 +38,18 @@
 
 # 92. [Interactive Coding Exercise] Grading Program
 
-student_scores = {
-  "Harry": 81,
-  "Ron": 78,
-  "Hermione": 99, 
-  "Draco": 74,
-  "Neville": 62,
-}
+# student_scores = {
+#   "Harry": 81,
+#   "Ron": 78,
+#   "Hermione": 99, 
+#   "Draco": 74,
+#   "Neville": 62,
+# }
 # 🚨 Don't change the code above 👆
 
 
 #TODO-1: Create an empty dictionary called student_grades.
-student_grades = {}
+# student_grades = {}
 
 
 #TODO-2: Write your code below to add the grades to student_grades.👇
@@ -58,19 +58,19 @@ student_grades = {}
 # Scores 71 - 80: Grade = "Acceptable"
 # Scores 70 or lower: Grade = "Fail"
 
-for student in student_scores:
-    if student_scores[student] >= 91:
-        student_grades[student] = "Outstanding"
-    elif student_scores[student] >= 81 and student_scores[student] < 90:
-        student_grades[student] = "Exceeds Expectations"
-    elif student_scores[student] >= 71 and student_scores[student] < 80:
-        student_grades[student] = "Acceptable"
-    else:
-        student_grades[student] = "Fail"
+# for student in student_scores:
+#     if student_scores[student] >= 91:
+#         student_grades[student] = "Outstanding"
+#     elif student_scores[student] >= 81 and student_scores[student] < 90:
+#         student_grades[student] = "Exceeds Expectations"
+#     elif student_scores[student] >= 71 and student_scores[student] < 80:
+#         student_grades[student] = "Acceptable"
+#     else:
+#         student_grades[student] = "Fail"
 
 
-# 🚨 Don't change the code below 👇
-print(student_grades)
+# # 🚨 Don't change the code below 👇
+# print(student_grades)
 # -------------------------------------
 
 
