e11155900a801d7cc0b2b8f4540e606d5dd4ed9d
krn <contadorkrn@gmail.com>
2023-06-21 20:12:04 -0300

    Lesson 102 Day 010

--- Day 010/main.py
+++ Day 010/main.py
@@ -28,3 +28,55 @@
 #     format_name(input("What is your first name? "), input("What is your last name? "))
 # )
 # -------------------------------------
+
+
+# 101. [Interactive Coding Exercise] Days in Month
+
+
+# def is_leap(year):
+#     if year % 4 == 0:
+#         if year % 100 == 0:
+#             if year % 400 == 0:
+#                 return True
+#             else:
+#                 return False
+#         else:
+#             return True
+#     else:
+#         return False
+
+
+# def days_in_month(year, month):
+#     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#     if is_leap(year) == True:
+#         month_days[1] = 29
+#         return month_days[month - 1]
+#     return month_days[month - 1]
+
+
+# # 🚨 Do NOT change any of the code below
+# year = int(input("Enter a year: "))
+# month = int(input("Enter a month: "))
+# days = days_in_month(year, month)
+# print(days)
+# -------------------------------------
+
+
+# 102. Docstrings
+
+
+# def format_name(f_name, l_name):
+#     """Take a first and last name and format it to return the title case version of the name."""
+
+#     if f_name == "" or l_name == "":
+#         return
+#     formated_f_name = f_name.title()
+#     formated_l_name = l_name.title()
+
+#     return f"Result: {formated_f_name} {formated_l_name}"
+
+
+# print(
+#     format_name(input("What is your first name? "), input("What is your last name? "))
+# )
+# -------------------------------------
