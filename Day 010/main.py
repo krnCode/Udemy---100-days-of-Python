665f20b26ec8c7a57a7167f2038e49dfa062dfb0
krn <contadorkrn@gmail.com>
2023-06-23 13:58:52 -0300

    Lesson 106 Day 010 + Calculator project

--- Day 010/main.py
+++ Day 010/main.py
@@ -187,3 +187,133 @@
 
 # print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
 # -------------------------------------
+
+
+# 105. While Loops, Flags and Recursion
+
+
+# # Add
+# def add(n1, n2):
+#     return n1 + n2
+
+
+# # Subtract
+# def subtract(n1, n2):
+#     return n1 - n2
+
+
+# # Multiply
+# def multiply(n1, n2):
+#     return n1 * n2
+
+
+# # Divide
+# def divide(n1, n2):
+#     return n1 / n2
+
+
+# # Calculations
+# operations = {
+#     "+": add,
+#     "-": subtract,
+#     "*": multiply,
+#     "/": divide,
+# }
+
+
+# def calculator():
+#     num1 = int(input("What's the first number? "))
+
+#     for symbol in operations:
+#         print(symbol)
+
+#     should_continue = True
+
+#     while should_continue:
+#         operation_symbol = input("Pick an operation: ")
+#         num2 = int(input("What's the next number? "))
+#         calculation_function = operations[operation_symbol]
+#         answer = calculation_function(num1, num2)
+
+#         print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+#         calc_continue = input(
+#             f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: "
+#         )
+
+#         if calc_continue == "n":
+#             should_continue = False
+#             calculator()
+
+#         else:
+#             num1 = answer
+
+
+# calculator()
+# -------------------------------------
+
+
+# 106. Calculator Finishing Touches and Bug Fixes
+# from art import logo
+
+
+# # Add
+# def add(n1, n2):
+#     return n1 + n2
+
+
+# # Subtract
+# def subtract(n1, n2):
+#     return n1 - n2
+
+
+# # Multiply
+# def multiply(n1, n2):
+#     return n1 * n2
+
+
+# # Divide
+# def divide(n1, n2):
+#     return n1 / n2
+
+
+# # Calculations
+# operations = {
+#     "+": add,
+#     "-": subtract,
+#     "*": multiply,
+#     "/": divide,
+# }
+
+
+# def calculator():
+#     print(logo)
+#     num1 = float(input("What's the first number? "))
+
+#     for symbol in operations:
+#         print(symbol)
+
+#     should_continue = True
+
+#     while should_continue:
+#         operation_symbol = input("Pick an operation: ")
+#         num2 = float(input("What's the next number? "))
+#         calculation_function = operations[operation_symbol]
+#         answer = calculation_function(num1, num2)
+
+#         print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+#         calc_continue = input(
+#             f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: "
+#         )
+
+#         if calc_continue == "n":
+#             should_continue = False
+#             calculator()
+
+#         else:
+#             num1 = answer
+
+
+# calculator()
+# -------------------------------------
